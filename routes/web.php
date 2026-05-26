a93f617c465bfb4c12293d673e1ac0a25aecc618
M. Donny Irwansyah <irwansyah.jr27@gmail.com>
2026-05-26 11:11:23 +0700

    feat: add user route to return paginated user data as JSON

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/user', function () {
+    return response()->json([
+        'data' => User::paginate(10)
+    ]);
 });
